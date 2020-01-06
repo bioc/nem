c0752d3f0b4316f7317cc458140df4fee439a54d
lshep <lori.shepherd@roswellpark.org>
2020-01-06 11:45:45 -0500

    Deprecate package

--- R/zzz.R
+++ R/zzz.R
@@ -7,3 +7,9 @@
 .onLoad <- function(lib, pkgname){
 	library.dynam(pkgname, pkgname, lib)
 }
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.12")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
